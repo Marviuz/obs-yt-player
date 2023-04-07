fe880396c8c91e0ec0072841b53cea339da9de5f
Marviuz <marviuz.gaming@gmail.com>
2023-04-07 14:19:04 +0800

    renamed "on-redeem" to "on-command"

--- src/pages/on-command/index.tsx
+++ src/pages/on-command/index.tsx
@@ -16,7 +16,7 @@ export const getServerSideProps = async ({ query }: NextPageContext) => {
   };
 };
 
-export default function OnRedeem({
+export default function OnCommand({
   limit,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
   const userInteraction = useComfyCommand();
