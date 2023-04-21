02cbea434f04f316bd862c7132a0260f82a8f071
Marviuz <marviuz.gaming@gmail.com>
2023-04-21 16:16:51 +0800

    fix yt input

--- src/server/routers/_app.ts
+++ src/server/routers/_app.ts
@@ -1,12 +1,23 @@
-import { search } from 'play-dl';
+import {
+  search,
+  yt_validate,
+  YouTubeVideo,
+  extractID,
+  video_info,
+} from 'play-dl';
 import { z } from 'zod';
 
 import { procedure, router } from '../trpc';
 
 export const appRouter = router({
-  searchVid: procedure
-    .input(z.string())
-    .mutation(async ({ input }) => await search(input)),
+  searchVid: procedure.input(z.string()).mutation(async ({ input }) => {
+    if (input.startsWith('https') && yt_validate(input) === 'video') {
+      const vid = await video_info(input);
+      return [vid.video_details];
+    }
+
+    return await search(input);
+  }),
 });
 
 export type AppRouter = typeof appRouter;
