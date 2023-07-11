674e97046fd9267346a87e5cb687a451dcb194cd
Marviuz <marviuz.gaming@gmail.com>
2023-07-12 03:11:28 +0800

    add adsense

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -3,6 +3,7 @@ import { Form, Formik, FormikHelpers, FormikValues } from 'formik';
 import { AnimatePresence, motion } from 'framer-motion';
 import Head from 'next/head';
 import Image from 'next/image';
+import Script from 'next/script';
 import { useCallback, useState } from 'react';
 
 import CopyIcon from '@/Icons/Copy';
@@ -364,6 +365,11 @@ export default function Home() {
         </div>
       </main>
       <Footer />
+      <Script
+        async
+        src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.NEXT_PUBLIC_GOOGLE_ADSENSE_CLIENT}`}
+        crossOrigin="anonymous"
+      />
     </>
   );
 }
